da030acbe033a8d38b42c864dab867e4e69ba831
Tarun Gorre <tarungorre@Taruns-MacBook-Pro.local>
2017-09-23 19:04:41 +0200

    Initial Commit

---
+++ SendDocs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendDocs
+//
+//  Created by Walkingcoin Admin on 19/09/17.
+//  Copyright © 2017 VSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
